6704cc8d38ffae695f74638dd8454ace0bc3fee7
Oliver <oliver.kali.hub@gmail.com>
2023-08-26 15:41:06 -0600

    fix: paths

--- src/components/GridCardCompany/GridCardCompany.tsx
+++ src/components/GridCardCompany/GridCardCompany.tsx
@@ -5,20 +5,22 @@ import { CardCompany } from '../CardCompany/CardCompany'
 export const GridCardCompany = () => {
   
   return (
-    <div className='gridCardCompany global-container'>
-      {
-        companyEmploymentData.map(item => (
-          <CardCompany
-            contract={item.contract}
-            nameComapany={item.company}
-            location={item.location}
-            postedAt={item.postedAt}
-            logo={item.logo}
-            position={item.position}
-            logoBackground={item.logoBackground}
-          />
-        ))
-      }
+    <div className='global-container'>
+      <div className='gridCardCompany '>
+        {
+          companyEmploymentData.map(item => (
+            <CardCompany
+              contract={item.contract}
+              nameComapany={item.company}
+              location={item.location}
+              postedAt={item.postedAt}
+              logo={item.logo}
+              position={item.position}
+              logoBackground={item.logoBackground}
+            />
+          ))
+        }
+      </div>
     </div>
   )
 }
